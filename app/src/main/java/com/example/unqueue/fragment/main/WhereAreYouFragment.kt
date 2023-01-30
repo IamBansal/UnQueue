119e52f301947cf763795605b7733af0e89cec28
IamBansal <abansal11ae@gmail.com>
2023-01-30 16:17:24 +0530

    UI Fix

--- app/src/main/java/com/example/unqueue/fragment/main/WhereAreYouFragment.kt
+++ app/src/main/java/com/example/unqueue/fragment/main/WhereAreYouFragment.kt
@@ -1,5 +1,6 @@
 package com.example.unqueue.fragment.main
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -27,6 +28,24 @@ class WhereAreYouFragment : Fragment() {
         binding.btnNext.setOnClickListener {
             generateQID()
         }
+
+        binding.ivLogout.setOnClickListener {
+            logout()
+        }
+
+    }
+
+    private fun logout() {
+
+        val alert = AlertDialog.Builder(requireContext())
+        alert.setTitle("Logout Requested!!")
+            .setMessage("You sure you want to logout?")
+            .setPositiveButton("Yes, logout"){_,_->
+
+            }
+            .setNegativeButton("No"){_,_->}
+            .create()
+            .show()
 
     }
 
